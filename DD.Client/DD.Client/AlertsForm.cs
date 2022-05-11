ff693d0939886304b541b6213a48438976554b3a
OranGeNaL <pshenichniy2514@gmail.com>
2022-05-11 14:03:29 +1000

    Начал переделывать на WPF

--- DD.Client/DD.Client/AlertsForm.cs
+++ DD.Client/DD.Client/AlertsForm.cs
@@ -14,6 +14,7 @@ namespace DD.Client
     public partial class AlertsForm : Form
     {
         private List<AlertWidget> alertWidgets = new List<AlertWidget>();
+        private int startPosition = 15;
 
         public AlertsForm()
         {
@@ -25,17 +26,38 @@ namespace DD.Client
             this.BackColor = Color.FromArgb(255, 183, 183);
             this.FormClosed += BeforeClosing;
 
-            alertWidgets.Add(new AlertWidget(new Alert()));
+            alertWidgets.Add(new AlertWidget(new Alert() { Description = "Lorem ipsum dolor sid amet more words just to check how this will display" }));
+            alertWidgets.Add(new AlertWidget(new Alert() { Description = "Недоступен хост hostname.aqua\nlast aviability: 22:14:13\nhostname: hostname.aqua\nСсылка на инструкцию: link\nСсылка на мониторинг: link"}));
+            alertWidgets.Add(new AlertWidget(new Alert() { Status = 2 }));
 
             foreach (var alertWidget in alertWidgets)
             {
                 this.Controls.Add(alertWidget.panel);
             }
+
+            UpdateAlertWidgets();
         }
 
         private void BeforeClosing(object sender, FormClosedEventArgs e)
         {
             
+        }
+
+
+
+        private void UpdateAlertWidgets()
+        {
+            this.Controls.Clear();
+
+            int position = startPosition;
+
+            foreach (var alertWidget in alertWidgets)
+            {
+                alertWidget.panel.Location = new Point(15, position);
+                this.Controls.Add(alertWidget.panel);
+
+                position += alertWidget.panel.Height + 20;
+            }
         }
     }
 }
