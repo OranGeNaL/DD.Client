ec4910bddd27a1becb4115c7e690b1201208e084
OranGeNaL <pshenichniy2514@gmail.com>
2022-05-11 22:10:02 +1000

    Виджеты алертов работают

--- DD.ClientWPF/DD.ClientWPF/AlertWidgetController.cs
+++ DD.ClientWPF/DD.ClientWPF/AlertWidgetController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DD.ClientWPF
 {
@@ -14,14 +17,60 @@ namespace DD.ClientWPF
 
         public AlertWidgetController() { }
 
+        private bool expended = false;
+
         public AlertWidgetController(Alert alert)
         {
             AlertData = alert;
             AlertWidget = new AlertWidget();
 
+            UpdateWidgetContent();
+
+            AlertWidget.ExpandCollapseButton.Click += ExpandCollapseButton_Click;
+        }
+
+        public void UpdateWidgetContent()
+        {
+            switch (AlertData.Status)
+            {
+                case 0:
+                    AlertWidget.TakeButton.Content = AlertWidget.TryFindResource("TakeAlert");
+                    AlertWidget.Worker.Content = "Не в работе";
+                    break;
+                case 1:
+                    if (AlertData.Worker != ParametersKeeper.UserName)
+                        AlertWidget.TakeButton.Content = AlertWidget.TryFindResource("TakeAlert");
+                    else
+                        AlertWidget.TakeButton.Content = AlertWidget.TryFindResource("CloseAlert");
+                    AlertWidget.Worker.Content = "В работе: " + AlertData.Worker;
+                    break;
+                case 2:
+                    AlertWidget.TakeButton.Visibility = System.Windows.Visibility.Collapsed;
+                    AlertWidget.AlertHeader.Width = 370;
+                    AlertWidget.Container.Background = (Brush)AlertWidget.TryFindResource("ClosedAlertBackColor");
+                    AlertWidget.Worker.Content = "Закрыт: " + AlertData.Worker;
+                    break;
+            }
 
             AlertWidget.AlertHeader.Content = AlertData.Header;
-            AlertWidget.AlertDescription.Content = AlertData.Description;
+            AlertWidget.AlertDescriptionContent.Text = AlertData.Description;
+        }
+
+        private void ExpandCollapseButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if(expended)
+            {
+                expended = false;
+                AlertWidget.AlertDescriptionContent.MaxHeight = 100;
+
+                AlertWidget.ExpandCollapseButton.Content = AlertWidget.TryFindResource("ExpandDescription");
+            }
+            else
+            {
+                expended = true;
+                AlertWidget.AlertDescriptionContent.MaxHeight = 600;
+                AlertWidget.ExpandCollapseButton.Content = AlertWidget.TryFindResource("CollapseDescription");
+            }
         }
     }
 }
---
+++ DD.ClientWPF/DD.ClientWPF/ParametersKeeper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DD.ClientWPF
+{
+    public static class ParametersKeeper
+    {
+        public static int SystemIndex = 0;
+
+        public static string UserName = "User User";
+
+        public const string GetAllAlerts = "";
+        public const string GetAlert = "";
+    }
+}
